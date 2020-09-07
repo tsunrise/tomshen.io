f5f88014427c9ffd0ddf0d5456730b5b4fb54e79
Tom Shen <tomshen@berkeley.edu>
2020-09-07 15:46:19 -0700

    update linear-sumcheck docs

--- personal/libra_sumcheck_sample_docs/implementors/algebra_core/error/trait.Error.js
+++ personal/libra_sumcheck_sample_docs/implementors/algebra_core/error/trait.Error.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["libra_sumcheck"] = [{"text":"impl <a class=\"trait\" href=\"algebra_core/error/trait.Error.html\" title=\"trait algebra_core::error::Error\">Error</a> for <a class=\"enum\" href=\"libra_sumcheck/enum.Error.html\" title=\"enum libra_sumcheck::Error\">Error</a>","synthetic":false,"types":["libra_sumcheck::error::Error"]}];
+implementors["linear_sumcheck"] = [{"text":"impl <a class=\"trait\" href=\"algebra_core/error/trait.Error.html\" title=\"trait algebra_core::error::Error\">Error</a> for <a class=\"enum\" href=\"linear_sumcheck/enum.Error.html\" title=\"enum linear_sumcheck::Error\">Error</a>","synthetic":false,"types":["linear_sumcheck::error::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ personal/libra_sumcheck_sample_docs/linear_sumcheck/ml_sumcheck/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["t13","t13 implementation of linear sumcheck"]],"trait":[["MLSumcheck","ML sumcheck protocol"],["MLSumcheckClaim","Claim of MLSumcheck"],["MLSumcheckSubclaim","subclaim of the MLSumcheck"]]});
